7578e676bac88f50c3c7342a8ef30428b06e5769
Noah <z5271292@ad.unsw.edu.au>
2021-08-20 01:30:59 +0800

    hardcode wks

--- js/script.js
+++ js/script.js
@@ -7,22 +7,22 @@ function fTime(t) {
     return strftime('%H:%M:%S.%L', t);
 }
 
-function getSelectedCode(a) {
-    return $(`[data-select="${a}"] option:selected`).val();
-}
+// function getSelectedCode(a) {
+//     return $(`[data-select="${a}"] option:selected`).val();
+// }
 
-function enableStationSelector() {
-    var selectedLineCode = getSelectedCode('line');
-    $('[data-select="station"]').empty().append('<option selected="selected" disabled="disabled">Select station</option>')
-    for (const line of lines) {
-        if (line.code == selectedLineCode) {
-            for (const station of line.stations) {
-                $('[data-select="station"]').append(`<option value="${station.code}">${station.name}</option>`);
-            }
-        }
-    }
-    $('[data-select="station"]').attr("disabled", false);
-}
+// function enableStationSelector() {
+//     var selectedLineCode = getSelectedCode('line');
+//     $('[data-select="station"]').empty().append('<option selected="selected" disabled="disabled">Select station</option>')
+//     for (const line of lines) {
+//         if (line.code == selectedLineCode) {
+//             for (const station of line.stations) {
+//                 $('[data-select="station"]').append(`<option value="${station.code}">${station.name}</option>`);
+//             }
+//         }
+//     }
+//     $('[data-select="station"]').attr("disabled", false);
+// }
 
 function getStationNameByCode(code) {
     for (const line of lines) {
@@ -33,17 +33,13 @@ function getStationNameByCode(code) {
     return 'Unknown Station'
 }
 
-for (const line of lines) {
-    $('[data-select="line"]').append(`<option value="${line.code}">${line.name}</option>`);
-}
-enableStationSelector();
-$('[data-select="line"]').change(() => {
-    enableStationSelector();
-})
-
-// function convertDateToUTC(date) { 
-//     return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); 
+// for (const line of lines) {
+//     $('[data-select="line"]').append(`<option value="${line.code}">${line.name}</option>`);
 // }
+// enableStationSelector();
+// $('[data-select="line"]').change(() => {
+//     enableStationSelector();
+// })
 
 class Eta {
     constructor(data) {
@@ -71,7 +67,7 @@ class Stop {
                 break;
             }
         }
-        this.lang = lang;
+        this.lang = 'EN';
     }
 
     async getEta() {
@@ -111,7 +107,7 @@ class Stop {
                 $('[data-section="DOWN"]').removeClass("hidden");
             }
 
-            console.log(lastEta)
+            // console.log(lastEta)
             $('#progressbar').css('opacity', '100%').animate({
                 opacity: 0
             }, 250);
@@ -192,115 +188,13 @@ class Stop {
     }
 }
 
-$('#get').click(() => {
-    const interval = 2000;
-    setInterval(() => {
-        let stop = new Stop(getSelectedCode('line'), getSelectedCode('station'))
-        stop.getEta();
+const interval = 2000;
+setInterval(() => {
+    let stop = new Stop('TML', 'WKS')
+    stop.getEta();
+}, interval);
 
-    }, interval);
-});
-
+//timer
 setInterval(() => {
     $('[data-time="comp"]').html(fTime(new Date()));
 }, 50);
-
-
-// class Eta {
-//     constructor() {}
-//     setGeneratedTime(t) {
-//         this.generated = new Date(t)
-//     }
-//     fromStopEta(o) {
-//         let route = new Route();
-//         route.fromStopEta(o);
-//         this.route = route;
-//         this.eta = new Date(o['eta']);
-//         this.eta_sequence = o['eta_seq'];
-//         this.remark_en = o['rmk_en'];
-//         this.remark_tc = o['rmk_tc'];
-//         this.lastUpdated = new Date(o['data_timestamp']);
-//         this.delta = convertDateToUTC(new Date(this.eta - new Date()));
-//         this.valid = this.eta > new Date();
-//     }
-// }
-
-// class Route {
-//     constructor() {}
-//     fromStopEta(o) {
-//         this.destination_en = o['dest_en'];
-//         this.destination_tc = o['dest_tc'];
-//         this.direction = o['dir'];
-//         this.route = o['route'];
-//         this.route_sequence = o['seq'];
-//     }
-// }
-
-// class Stop {
-//     constructor(stopId) {
-//         this.stopId = stopId;
-//         this.etas = [];
-//     }
-
-//     // gets eta of all routes passing through that stop
-//     async getEta() {
-//         let response = await fetch(baseURL+'v1/transport/kmb/stop-eta/'+this.stopId);
-//         this.response = await response.json()
-//         return this;
-//     }
-
-//     processEta() {
-//         for (const data of this.response.data) {
-//             let eta = new Eta()
-//             eta.setGeneratedTime(this.response.generated_timestamp)
-//             eta.fromStopEta(data)
-//             this.etas.push(eta)
-//         }
-//         this.etas.sort((a, b) => a.eta - b.eta)
-//         return this;
-//     }
-
-//     updateInfo(tblNum) {
-//         let html = '';
-//         for (const data of this.etas) {
-//             if (data.valid) {
-//                 let colour = data.remark_en == '' ? 'table-success' : 'table-secondary'
-//                 html += `<tr class="${colour}">
-//                     <td>${data.route.route}</td>
-//                     <td>${data.route.destination_en}</td>
-//                     <td>${strftime('%H:%M:%S', data.eta)}</td>
-//                     <td>${strftime('%H:%M:%S', data.delta)}</td>
-//                     <td>${data.remark_en}</td>
-//                 </tr>`;
-//             }
-//         }
-//         $(`#table${tblNum}`).html(html);
-//         $(`#generated${tblNum}`).html(strftime('%H:%M:%S', new Date(this.response.generated_timestamp)))
-//     }
-// }
-
-// // main code
-// let stopNum1 = 0;
-// let stopNum2 = 1;
-
-// setInterval(function() {
-//     let stop1 = new Stop(defaultStops[stopNum1].id);
-//     stop1.getEta()
-//     .then(() => stop1.processEta())
-//     .then(() => stop1.updateInfo(1))
-//     .then(() => {
-//         // update title and subtitle
-//         $(`#destination1`).html(defaultStops[stopNum1].description);
-//         $(`#bound1`).html(defaultStops[stopNum1].bound);
-//     });
-
-//     let stop2 = new Stop(defaultStops[stopNum2].id);
-//     stop2.getEta()
-//     .then(() => stop2.processEta())
-//     .then(() => stop2.updateInfo(2))
-//     .then(() => {
-//         // update title and subtitle
-//         $(`#destination2`).html(defaultStops[stopNum2].description);
-//         $(`#bound2`).html(defaultStops[stopNum2].bound);
-//     });
-// }, 1000);
